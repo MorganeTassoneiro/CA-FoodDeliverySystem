ba5f298012684344fba7998ed2535d4907dea9d4
MorganeTassoneiro <2021327@student.cct.ie>
2023-11-27 15:11:16 +0000

    managing the system of orders

--- contracts/Chain.sol
+++ contracts/Chain.sol
@@ -1,28 +1,45 @@
+// SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.7.0 <0.9.0;
 
 import './Order.sol';
 
 /**
  * @title Chain
- * @dev Make, accept, reject orders
+ * @dev Manage the creation, acceptance, and rejection of orders
  */
 contract Chain {
-    mapping(uint256 => Order) private orderMapping;
-    uint256 private latestOrderIndex = 0;
+    mapping(uint256 => Order) private orderInstances;
+    uint256 private lastOrderId = 0;
 
-    function createOrder(address _restaurant, uint256 _deliveryPrice, uint256 _total, string memory _items, string memory _clientItems) public payable returns (uint256) {
-        latestOrderIndex++;
-        orderMapping[latestOrderIndex] = new Order{value: msg.value}(latestOrderIndex, msg.sender, _restaurant, _deliveryPrice, _total, _items, _clientItems);
-        return latestOrderIndex;
+    /**
+     * @dev Create a new order
+     * @param _restaurant Address of the restaurant
+     * @param _deliveryPrice Delivery price for the order
+     * @param _total Total cost of the order
+     * @param _items List of items in the order
+     * @param _itemsClient List of items for the client
+     * @return The unique identifier of the created order
+     */
+    function makeOrder(address _restaurant, uint256 _deliveryPrice, uint256 _total, string memory _items, string memory _itemsClient) public payable returns (uint256) {
+        lastOrderId++;
+        orderInstances[lastOrderId] = new Order{value: msg.value}(lastOrderId, msg.sender, _restaurant, _deliveryPrice, _total, _items, _itemsClient);
+        return lastOrderId;
     }
-}   
- function getAddressByOrder(uint256 orderIndex) public view returns (address) {
-        return address(orderMapping[orderIndex]);
+
+    /**
+     * @dev Find the address of a specific order
+     * @param orderId The unique identifier of the order
+     * @return The address of the specified order
+     */
+    function findOrderAddress(uint256 orderId) public view returns (address) {
+        return address(orderInstances[orderId]);
     }
-    
-    function getLatestOrderIndex() public view returns (uint256) {
-        return latestOrderIndex;
+
+    /**
+     * @dev Get the index of the last order
+     * @return The index of the last order
+     */
+    function getLastOrderId() public view returns (uint256) {
+        return lastOrderId;
     }
-
-
-
+}
